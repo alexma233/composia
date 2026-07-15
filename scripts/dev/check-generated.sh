4ffe94fab5e3e0e583ec45b1a141bcf3316037e6
alexma233 <i@fur.im>
2026-07-15 22:22:28 +0800

    fix ci generated drift environment

--- scripts/dev/check-generated.sh
+++ scripts/dev/check-generated.sh
@@ -8,14 +8,11 @@ cd "$ROOT_DIR"
 GOBIN_DIR=$(mktemp -d)
 trap 'rm -rf "$GOBIN_DIR"' EXIT
 
+BUF_VERSION=$(sed -n 's/^buf = "\([^"]*\)"$/\1/p' mise.toml)
 PROTOBUF_VERSION=$(go list -m -f '{{.Version}}' google.golang.org/protobuf)
 CONNECT_VERSION=$(go list -m -f '{{.Version}}' connectrpc.com/connect)
 
-if ! command -v buf >/dev/null 2>&1; then
-  printf '%s\n' 'buf is required to generate protobuf code.' >&2
-  exit 1
-fi
-
+GOBIN="$GOBIN_DIR" go install "github.com/bufbuild/buf/cmd/buf@v$BUF_VERSION"
 GOBIN="$GOBIN_DIR" go install "google.golang.org/protobuf/cmd/protoc-gen-go@$PROTOBUF_VERSION"
 GOBIN="$GOBIN_DIR" go install "connectrpc.com/connect/cmd/protoc-gen-connect-go@$CONNECT_VERSION"
 
